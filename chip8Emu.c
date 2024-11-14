030b107391d95a92f6e05dd67cce286474109818
Ahamd <ajs12901@hotmail.com>
2024-11-13 21:12:06 -0500

    Add gitignore file

--- chip8Emu.c
+++ chip8Emu.c
@@ -111,3 +111,27 @@ int main (int argc, char **argv)
   cleanup_sdl(&sdl_parameters);
   return 0; 
 }
+
+
+
+
+
+/*
+// MAC requires SDL_EVENT in order to show a window:
+bool quit = false;
+SDL_Event sdl_event;
+
+// While application is running
+while( !quit )
+{
+// Handle events on queue
+  while( SDL_PollEvent(&sdl_event) != 0) // poll for event
+  {
+      // User requests quit
+      if( sdl_event.type == SDL_QUIT ) // unless player manually quits
+      quit = true;
+  }
+
+update_window(&sdl_parameters, &config_parameters); 
+}
+*/
